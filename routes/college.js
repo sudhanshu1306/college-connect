a5bb17eba5614bd7ae1b8359e7be2215112cbace
sudhanshu1306 <54653437+sudhanshu1306@users.noreply.github.com>
2021-01-18 05:38:22 +0530

    Delete routes directory

--- routes/college.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {isAuthenticated} from "../controllers/registration.js";
-import {getNews} from "../controllers/college.js";
-
-const router=express.Router();
-
-router.get("/",isAuthenticated,getNews);
-
-export default router;
